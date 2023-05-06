c0bf9929b21420a4b36ec38f6019273d8ce1e5ed
mouadka <moaadkarkouri@gmail.com>
2023-05-06 23:52:09 +0200

    Added Main class with main method

---
+++ CanadaApp/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // instantiate candidate without parameters (calling empty constructor)
+        Candidate candidate = new Candidate();
+
+        // instantiate candidate with parameters (calling constructor with args)
+        Candidate candidate1 = new Candidate("Moaad",31,"Single",0,"Master");
+
+        
+    }
+}
