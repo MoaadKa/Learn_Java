edace5976e825c75ed883a9c22d10c175b19b8f0
mouadka <moaadkarkouri@gmail.com>
2023-05-15 10:54:09 +0200

    Added new Class Questions which will have all the questions in the form

---
+++ CanadaApp/src/Questions.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Questions {
+
+    public Questions() {
+        new Candidate();
+    }
+
+    Candidate candidate;
+
+    public void questions() {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter your full name: ");
+        candidate.setFullName(keyboard.nextLine());
+        System.out.println("How old are you: ");
+        candidate.setAge(keyboard.nextInt());
+        keyboard.nextLine();
+
+        System.out.println("What's your level of studies: ");
+        candidate.setDegree(keyboard.nextLine());
+
+        System.out.println("Family situation: ");
+        candidate.setSituation(keyboard.nextLine());
+        System.out.println("Number of children");
+        candidate.setNumberOfChildren(keyboard.nextInt());
+
+
+    }
+}
+
